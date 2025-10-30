d8e02d8e61b8927cfbdf69adc5fb27e610a0a2c7
Jeferson6191 <jeffrodjs0@gmail.com>
2025-10-29 22:25:34 -0300

    mudança de titulo do html, mudança de botões na navbar, desenvolvimento do layout da guia inicial (home)

--- js/templates.js
+++ js/templates.js
@@ -1,4 +1,13 @@
 let template = {
     indexhtml: `
+                <div class="card m-5 conteiner">
+            <div class="card-body mx-auto p-5">
+                <h1 class="card-title pb-3">Guia inicial</h1>
+                <p class="card-text">Este é um site de montagem e gerenciamento de treinos, desenvolvido com intuito que qualquer pessoa possas utilizar.</p>
+                <p class="caard-text">Abaixo há duas opções, a primeira tendo a função de ir diretamente para a aba de montagem de treinos e a segunda redirecionando o usuario para o guia de aprendizado.</p>
+                <button class="btn btn-primary mt-3" id="montar-treino-btn">Montar seu treino agora</button>
+                <button class="btn btn-primary ms-2 mt-3" id="guia-btn">Guia de aprendizado</button>
+            </div>
+            </div>
     `
 }
